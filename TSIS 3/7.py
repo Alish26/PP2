9fefb1a8b6d615f8d45694e935c091f85c111051
Aip Alisher <alish2621@gmail.com>
2020-06-18 19:06:25 +0600

    Fourth commit

--- TSIS 3/7.py
+++ TSIS 3/7.py
@@ -1,39 +1,19 @@
 n = int(input())
-
 l = []
-
 for i in range(n):
     ll = list(map(int, input().split()))
     l.append(ll)
 
-cnt = 0
-
-x = []
-x.append(-10000)
-y = []
-y.append(-10000)
+ans = 0
 
 for i in range(n):
+    r = 0
+    c = 0 
     for j in range(n):
-        if l[i][j] > 0:
-            cnt += 1
+        if l[i][j]: ans += 1
+        r = max(r, l[i][j])
+        c = max(c, l[j][i])
 
-for i in range(n):
-    for j in range(n):
-        if(x[len(x) - 1] < (l[i][n - 1 - j] - j)):
-            x[len(x) - 1] = (l[i][n - 1 - j] - j)
-        if i != 
-    x.append(-1000)
+    ans += r + c
 
-for i in range(n):
-    for j in range(n):
-        if y[len(y) - 1] < (l[n - j - 1][i] - j):
-            y[len(y) - 1] = (l[n -j - 1][i] - j)
-        if j != 0 and j != n - 1 and l[n - j][i] == 0 and y[len(y) - 1] < l[n - j - 1][i]:
-            y[len(y) - 1] = l[n - j - 1][i] 
-    y.append(-1000)
-
-for i in range(n):
-    cnt += x[i] + y[i]
-
-print(cnt)
+print(ans)
